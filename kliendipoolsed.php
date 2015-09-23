cd0004496c88533d4b8f5505c3aca3689f236b47
Heigo Kure <heigo.kure@khk.ee>
2015-09-23 12:01:53 +0300

    5. Kass koeraks [Delivers #103975986]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
 </head>
 <body>
 
@@ -13,7 +14,27 @@
         }
     </script>
 
-    <a href="www.khk.ee" onclick="return false;">J‰‰me siia!</a>
+    <a href="www.khk.ee" onclick="myFunction();return false">J‰‰me siia!</a>
+    <script>
+        function myFunction() {
+            alert("J‰‰me Siia")
+            "return false"
+        }
+    </script>
+
+    <img id="kass" src="media/cutecat.jpg">
+    <script>
+        $("img#kass").click(function () {
+            if($(this).attr("src") == "media/cutecat.jpg")
+            {
+                $(this).attr("src", "media/dog.jpg");
+            }
+            else
+            {
+                $(this).attr("src","media/cutecat.jpg");
+            }
+        });
+    </script>
 
 </body>
 </html>
