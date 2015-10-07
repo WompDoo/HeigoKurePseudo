8c8ec8487a9279d827a313fb253f82b226545c80
Heigo Kure <heigo.kure@khk.ee>
2015-10-07 19:40:15 +0300

    9. Parema klõpsu keelamine [Delivers #103975996]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -78,10 +78,9 @@
 <script>
     document.onmousedown=disableclick;
     status="Right Click Disabled";
-    function disableclick(event)
-    {
-        if(event.button==2)
-        {
+    function disableclick(event) {
+
+        if(event.button==2) {
             alert(status);
             return false;
         }
