52bc4ba2a8f7106842f0e914df1c0bf5196bed82
Heigo Kure <heigo.kure@khk.ee>
2015-11-18 13:39:26 +0200

    12. [Delivers #103976030] parandus

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,42 +1,4 @@
 
 <?php
 
-$isik = array(
-    "eesnimi => Heigo",
-    "perekonnanimi => Kure",
-    "vanus => 20",
-);
-echo "<p>" . $isik['eesnimi'] . " " . $isik['perekonnanimi'] . " (" . $isik['vanus'] . ")</p>";
-echo "<pre>";
-
-/*
-$eesnimi = "Heigo";
-$perekonnanimi = "Kure";
-$vanus = "20";
-echo "$eesnimi $perekonnanimi($vanus)";
-*/
-$heigo_algab_vokaaliga = false;
-echo $heigo_algab_vokaaliga;
-
-
-if ($heigo_algab_vokaaliga) {
-    echo "<p>Nimi algab vokaaliga</p>";
-};
-
-
-
-
-class Isik
-{
-    var $ees = "Heigo";
-    var $pere = "Kure";
-    var $vanus = 20;
-    var $sugu = "mees";
-}
-$heigo = new Isik;
-$heigo_algab_vokaaliga = false;
-echo "<p>" . $heigo->ees . " " . $heigo->pere . " (" . $heigo->vanus . ")</p>";
-if ($heigo_algab_vokaaliga) {
-    echo "<p>Nimi algab vokaaliga</p>";
-};
-
+echo "Tere Maailim";
