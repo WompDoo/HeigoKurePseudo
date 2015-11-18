db432315ab4392c727936bbbb066148064d4129c
Heigo Kure <heigo.kure@khk.ee>
2015-11-18 13:37:44 +0200

    16. [Delivers #103976030] parandus

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -24,3 +24,19 @@ if ($heigo_algab_vokaaliga) {
 };
 
 
+
+
+class Isik
+{
+    var $ees = "Heigo";
+    var $pere = "Kure";
+    var $vanus = 20;
+    var $sugu = "mees";
+}
+$heigo = new Isik;
+$heigo_algab_vokaaliga = false;
+echo "<p>" . $heigo->ees . " " . $heigo->pere . " (" . $heigo->vanus . ")</p>";
+if ($heigo_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+};
+
