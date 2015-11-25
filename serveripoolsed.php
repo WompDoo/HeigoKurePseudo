84c4cdc4ce725e145f119ea168161d35ee5a0421
Heigo Kure <heigo.kure@khk.ee>
2015-11-25 12:53:49 +0200

    21. [Delivers #103976022] parandus

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,20 +1,52 @@
 <?php
-class isik
-{
-    var $firstname = "Heigo";
-    var $lastname = "Kure";
-    var $age = 20;
-    var $sex = "<br /> Mees";
-}
+$eesnimi = "Heigo";
+$perekonnanimi = "Kure";
+$vanus = "20";
+echo "$eesnimi $perekonnanimi ($vanus)";
 $heigo = new isik;
 $vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
-if (in_array(substr($heigo->firstname, 0, 1), $vowels)) {
+if (in_array(substr($heigo->eesnimi, 0, 1), $vowels)) {
     $eesnimi_algab_vokaaliga = true;
 } else {
     $eesnimi_algab_vokaaliga = false;
 }
+echo "<br>";
+class isik
+{
+    var $eesnimi = "Heigo";
+    var $perekonnanimi = "Kure";
+    var $vanus = 21;
+    var $sugu = "Mees";
+}
+$heigo = new isik;
+echo "" . $heigo->eesnimi . " " . $heigo->perekonnanimi . " (" . $heigo->vanus . ") " . $heigo->sugu . " ";
 if ($eesnimi_algab_vokaaliga) {
     echo "<p>Nimi algab vokaaliga</p>";
 };
-echo "<p>" . $heigo->firstname . " " . $heigo->lastname . " (" . $heigo->age . ") " . $heigo->sex . "</p>";
+echo "<br>";
+$a = 1.23;
+$b = 2.34;
+$float_sum= $a + $b;
+echo " $float_sum ";
+echo $_GET['koer'];
 ?>
+
+<form action="?" method="post">
+    <input name="kass">
+    <input type="submit">
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
